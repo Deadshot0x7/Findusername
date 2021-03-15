c84bc82ae0e92f709bf3c89babd95f20d4e35db1
lucifer0x7 <55378960+Lucifer0x7@users.noreply.github.com>
2021-03-15 19:03:33 +0530

    New code
    Added Example file

--- username.py
+++ username.py
@@ -1,22 +1,23 @@
 #author : Sayyed Viquar ahmed 
 import requests
 import urllib
+import time
 
 
 def instagram(username):
     try:
         url="http://instagram.com/"+username
         print("Username found in  Instagram")
-        return("Username found ")
+        
     except Exception  as e :
             print("Can't find the Username ")
-            return("Can't find the the username ")
+            
 
 def pinrest(username):
     try:
         url="http://pinrest.com/"+username
         print("Username found in Pinrest platform")
-        return("Username Found")
+        
     except Exception as e:
         print("cant find the username")
 
@@ -24,14 +25,15 @@ def facebook(username):
     try:
         url="http://facebook.com/"+username
         print("Username found in Pinrest platform")
-        return("Username Found")
+        
     except Exception as e:
         print("cant find the username")
     
 
 
 
-    
+
+
         
 
 
